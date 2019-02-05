a38c95a078c15492a257b4c61c0e579bdd60f72e
Joshua Peek <josh@joshpeek.com>
2019-02-04 20:20:09 -0800

    Reboot from existing image
    Closes #9

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,14 +2,15 @@
 
 set -ex
 
-if [ -e $ROOTFS.tar.* ]; then
-  extract-tar.sh "$ROOTFS" $ROOTFS.tar.*
-fi
+if [ ! -e "$HDA_QCOW2" ]; then
+  if [ -e $ROOTFS.tar.* ]; then
+    extract-tar.sh "$ROOTFS" $ROOTFS.tar.*
+  fi
 
-detect-kernel.sh
-
-if [ -e "$ROOTFS" ] && [ ! -e "$HDA_QCOW2" ]; then
-  create-image.sh "$ROOTFS"
+  if [ -e "$ROOTFS" ]; then
+    detect-kernel.sh
+    create-image.sh "$ROOTFS"
+  fi
 fi
 
 exec qemu.sh "$@"
